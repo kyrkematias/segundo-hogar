87e7e68ea72f4412fe0ab76499ed88d43f280fb0
Roberto Matías <roberto@shuttlecloud.com>
2023-12-04 21:00:20 -0300

    fix initial recomm
    Signed-off-by: Roberto Matías <roberto@shuttlecloud.com>

--- sh-front-main/src/components/FindRoommate/index.js
+++ sh-front-main/src/components/FindRoommate/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Center } from "@chakra-ui/react";
 import { Loading } from "components/commons/Loading";
 import { FindRoommateForm } from "./FindRoommateForm";
@@ -7,27 +7,72 @@ import { Results } from "./Results";
 import { SectionHeader } from "components/commons/SectionHeader";
 import { sections } from "config/sections";
 import { useAxios } from "hooks/utils/useAxios";
-
-let idPerson = 1;
-const URL = `${process.env.REACT_APP_API_URL_RECOMM}/recomendation/${idPerson}`;
+import { GET_PERSON_ID_BY_USER_EMAIL } from "client/gql/queries/users";
+import { useQuery, useApolloClient } from "@apollo/client";
+import axios from 'axios';
 
 export function FindRoommate() {
     const { findRoommate } = sections;
-    const { response, error, loading } = useAxios({ url: URL, method: "get" });
+    const [idPerson, setIdPerson] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [response, setResponse] = useState(null);
+    const client = useApolloClient();
 
-    return (<>
-        <FormArea>
-            <SectionHeader
-                section={findRoommate.section}
-                sectionTitle={findRoommate.title}
-            />
-            <FindRoommateForm />
-        </FormArea>
-        <Box width={"100%"} my={20}>
-            <Center>
-                {loading ? <Loading minH={"30vh"} size={"lg"} m={20} /> : <Results recomms={response?.data} />}
-            </Center>
-        </Box>
-    </>
-    )
+    useEffect(() => {
+        async function fetchData() {
+            const email = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).email : null;
+            try {
+                const { data } = await client.query({
+                    query: GET_PERSON_ID_BY_USER_EMAIL,
+                    variables: { email },
+                    fetchPolicy: "no-cache",
+                });
+                setIdPerson(data.sh_users[0].persons_id);
+            } catch (error) {
+                console.error('Error fetching person ID:', error);
+            }
+        }
+        fetchData();
+    }, [client]);
+
+    useEffect(() => {
+        async function FetchRecomms() {
+            try {
+                const { data } = axios.get(`${process.env.REACT_APP_API_URL_RECOMM}/recomendation/${idPerson}`)
+                .then((res) => {
+                    console.log("DATA axios", res.data)
+                    let listaRecomendaciones = res.data;
+                    setResponse(listaRecomendaciones);
+                    setLoading(false);
+                }
+                )
+            } catch (error) {
+                console.error('Error fetching recomms:', error);
+            }
+        }
+        if (idPerson) {
+            console.log("Buscando recomendaciones...")
+            console.log("ID PERSONA", idPerson);
+            FetchRecomms();
+        }
+    }, [idPerson]);
+
+    console.log("RESPONSE final", response);
+
+    return (
+        <>
+            <FormArea>
+                <SectionHeader
+                    section={findRoommate.section}
+                    sectionTitle={findRoommate.title}
+                />
+                <FindRoommateForm />
+            </FormArea>
+            <Box width={"100%"} my={20}>
+                <Center> {loading ? <Loading /> : <Results recomms={response?.data} />}
+                </Center>
+            </Box>
+        </>
+
+    );
 }
