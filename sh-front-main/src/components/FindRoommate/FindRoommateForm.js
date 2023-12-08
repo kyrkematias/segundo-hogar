d306718f17c3a582f04827d4b7df454309fbc4ca
Martín Matías <martin_matias@hotmail.com.ar>
2023-12-07 21:55:33 -0300

    Feat/roommates search (#6)
    * change
    
    Signed-off-by: Roberto Matías <roberto@shuttlecloud.com>
    
    * initial find roommate filter
    
    * find rommate filter; starting roommate view
    
    ---------
    
    Signed-off-by: Roberto Matías <roberto@shuttlecloud.com>
    Co-authored-by: Roberto Matías <roberto@shuttlecloud.com>

--- sh-front-main/src/components/FindRoommate/FindRoommateForm.js
+++ sh-front-main/src/components/FindRoommate/FindRoommateForm.js
@@ -11,6 +11,7 @@ import {
   RangeSliderTrack,
   RangeSliderFilledTrack,
   RangeSliderThumb,
+  Text,
   Tooltip,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
@@ -19,14 +20,25 @@ import { CustomButton } from "components/commons/CustomButton";
 import { useGetCities } from "hooks/utils/useGetCities";
 import { useGetStates } from "hooks/utils/useGetStates";
 import { useGetCareers } from "hooks/utils/useGetCareers";
+import { differenceInYears } from "date-fns";
 import { GET_STUDENTS } from "client/gql/queries/utils";
+import { StudentsCards } from "./StudentsCards";
 
 export function FindRoommateForm() {
   const [ageRange, setAgeRange] = useState([18, 40]);
   const [showStartTooltip, setShowStartTooltip] = useState(false);
   const [showEndTooltip, setShowEndTooltip] = useState(false);
+  const [noResults, setNoResults] = useState(false);
+  const [showStudentsCards, setShowStudentsCards] = useState(false);
+  const [filters, setFilters] = useState({});
+  const [selectedGender, setSelectedGender] = useState("");
+  const [selectedCareer, setSelectedCareer] = useState("");
+  const [selectedState, setSelectedState] = useState(null);
+  const [parsedSelectedState, setParsedSelectedState] = useState(null);
+  const [selectedAge, setSelectedAge] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null)
   const { loading, error, data: students } = useQuery(GET_STUDENTS);
-  
+
   const {
     handleSubmit,
     register,
@@ -35,12 +47,51 @@ export function FindRoommateForm() {
 
   const searchSubmit = async (filters) => {
     filters.ageRange = ageRange;
-    
-    console.log(filters);
-    console.log("estudiantes: ", students)
-    
+    filters.gender = selectedGender;
+    filters.career = selectedCareer;
+    filters.state = selectedState;
+    filters.birth_date = selectedAge;
+    filters.city = selectedCity;
+    const parsedSelectedState = parseInt(selectedState);
+    setParsedSelectedState(parsedSelectedState);
+    // console.log("filtros", filters);
+    // console.log("estudiantes: ", students);
+
+    const studentGender = students.sh_students.map(
+      (student) => student?.person?.gender
+    );
+    const studentCareers = students.sh_students.map(
+      (student) => student?.career?.name
+    );
+    const studentStates = students.sh_students.map(
+      (student) => student?.city?.state?.id
+    );
+    const studentBirthDates = students.sh_students.map(
+      (student) => new Date(student?.person?.birth_date)
+    );
+    const studentCity = students.sh_students.map(
+      (student) => student?.city?.id
+    );
+
+    const genderMatch = studentGender.includes(selectedGender);
+    const careerMatch = studentCareers.includes(selectedCareer);
+    const statesMatch = studentStates.includes(parsedSelectedState);
+    const cityMatch = studentCity.includes(selectedCity);
+    const ageMatch = studentBirthDates.every(
+      (birthDate) => {
+        const age = differenceInYears(new Date(), birthDate);
+        return age >= ageRange[0] && age <= ageRange[1];
+      }
+    );
+
+    const allMatches = genderMatch && careerMatch && statesMatch;
+
+    console.log(allMatches);
+
+    setFilters(filters);
+    setShowStudentsCards(allMatches);
+    setNoResults(!allMatches);
   };
-  
 
   const { states } = useGetStates();
   const { cities, setStateSelected } = useGetCities();
@@ -56,11 +107,12 @@ export function FindRoommateForm() {
             placeholder="Selecciona..."
             width={["100%", "100%", "98%", "98%", "98%"]}
             {...register("gender")}
+            onChange={(e) => setSelectedGender(e.target.value)}
             _focus={{ background: "none" }}
           >
-            <option value="Male">Masculino</option>
-            <option value="Female">Femenino</option>
-            <option value="Other">Otro</option>
+            <option value="male">Masculino</option>
+            <option value="female">Femenino</option>
+            <option value="other">Otro</option>
           </Select>
           <FormErrorMessage>
             {errors.gender && errors.gender.message}
@@ -74,11 +126,12 @@ export function FindRoommateForm() {
               name="career"
               placeholder="Selecciona..."
               {...register("career")}
+              onChange={(e) => setSelectedCareer(e.target.value)}
               _focus={{ background: "none" }}
             >
               {careers?.map((career) => {
                 return (
-                  <option key={career.id} value={career.id}>
+                  <option key={career.id} value={career.name}>
                     {career.name}
                   </option>
                 );
@@ -94,12 +147,14 @@ export function FindRoommateForm() {
               aria-label={["min", "max"]}
               colorScheme="blackAlpha"
               defaultValue={[18, 30]}
-              onChangeEnd={(range) => setAgeRange(range)}
+              onChangeEnd={(range) => {
+                setAgeRange(range);
+                setSelectedAge(range);
+              }}
             >
               <RangeSliderTrack>
                 <RangeSliderFilledTrack />
               </RangeSliderTrack>
-
               <Tooltip
                 hasArrow
                 bg="black"
@@ -141,7 +196,7 @@ export function FindRoommateForm() {
               placeholder="Selecciona..."
               {...register("state")}
               _focus={{ background: "none" }}
-              onChange={(e) => setStateSelected(e.target.value)}
+              onChange={(e) => setSelectedState(e.target.value)}
             >
               {states?.map((state) => {
                 return (
@@ -162,6 +217,7 @@ export function FindRoommateForm() {
               name="city"
               placeholder="Selecciona..."
               {...register("city")}
+              // onChange={(e) => setSelectedCity(e.target.value)}
               _focus={{ background: "none" }}
             >
               {cities?.map((city) => {
@@ -180,7 +236,7 @@ export function FindRoommateForm() {
 
         <Center m={8}>
           <CustomButton
-            handleClick={handleSubmit(searchSubmit)}
+            handleClick={handleSubmit(() => searchSubmit(filters))}
             type="submit"
             // isLoading={isSubmitting}
             loadingText="Buscando..."
@@ -189,6 +245,21 @@ export function FindRoommateForm() {
           />
         </Center>
       </form>
+      {showStudentsCards && !noResults ? (
+        <StudentsCards
+          filters={filters}
+          students={students.sh_students}
+          parsedSelectedState={parsedSelectedState}
+        />
+      ) : (
+        noResults && (
+          <Center>
+            <Text fontSize={"25px"} py={2} as={"em"} color={"gray"}>
+              No se encontraron coincidencias de búsqueda.
+            </Text>
+          </Center>
+        )
+      )}
     </Box>
   );
 }
--- sh-front-main/src/hooks/utils/useGetCities.js
+++ sh-front-main/src/hooks/utils/useGetCities.js
@@ -1,29 +1,31 @@
-import { useState } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { GET_CITIES } from 'client/gql/queries/utils';
+import { useState } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { GET_CITIES } from "client/gql/queries/utils";
 
 export function useGetCities() {
+  const [isSelectedCity, setIsSelectedCity] = useState(false);
 
-    const [isSelectedCity, setIsSelectedCity] = useState(false);
+  let cities = [];
+  let variables = { state_id: window.localStorage.getItem("stateSelected") };
 
-    let cities = [];
-    let variables = { "state_id": window.localStorage.getItem("stateSelected") };
+  const [searchCity, { loading, error, data: citiesFromApi }] = useLazyQuery(
+    GET_CITIES,
+    { variables }
+  );
 
-    const [searchCity, { loading, error, data: citiesFromApi }] = useLazyQuery(GET_CITIES, { variables });
-
-    const setStateSelected = (id) => {
-        if (id === "") {
-            window.localStorage.removeItem("stateSelected");
-            setIsSelectedCity(false);
-            return;
-        }
-        window.localStorage.setItem("stateSelected", id);
-        setIsSelectedCity(true);
-        searchCity();
+  const setStateSelected = (id) => {
+    if (id === "") {
+      window.localStorage.removeItem("stateSelected");
+      setIsSelectedCity(false);
+      return;
     }
+    window.localStorage.setItem("stateSelected", id);
+    setIsSelectedCity(true);
+    searchCity();
+  };
 
-    if (!isSelectedCity) cities = [];
-    cities = citiesFromApi?.sh_cities;
+  if (!isSelectedCity) cities = [];
+  cities = citiesFromApi?.sh_cities;
 
-    return { setStateSelected, loading, error, cities };
+  return { setStateSelected, loading, error, cities };
 }
