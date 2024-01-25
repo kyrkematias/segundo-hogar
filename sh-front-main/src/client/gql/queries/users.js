18a0b0183e046c462d28e28140856c41e49d66c3
kyrkematias <martin_matias@hotmail.com.ar>
2024-01-25 19:03:51 -0300

    added shared filter to recomm cards

--- sh-front-main/src/client/gql/queries/users.js
+++ sh-front-main/src/client/gql/queries/users.js
@@ -14,43 +14,43 @@ export const GET_USERS = gql`
 `;
 
 export const GET_STUDENT_USER_BY_ID = gql`
-    query GetUserById($id: Int) {
-      sh_users(where: {id: {_eq: $id}}) {
-        id
-        email
-        username
-        bio
-        avatar
-        person {
-          firstname
-          lastname
-          gender
-          students {
-            career {
-              name
-              id
-            }
-            city {
-              id
-              name
-              state_id
-            }
-            shared
-            file_number
+  query GetUserById($id: Int) {
+    sh_users(where: { id: { _eq: $id } }) {
+      id
+      email
+      username
+      bio
+      avatar
+      person {
+        firstname
+        lastname
+        gender
+        students {
+          career {
+            name
+            id
           }
-        }
-        user_category {
-          id
-          description
+          city {
+            id
+            name
+            state_id
+          }
+          shared
+          file_number
         }
       }
+      user_category {
+        id
+        description
+      }
     }
+  }
 `;
 
 // get user by mail
 export const GET_USER_BY_EMAIL = gql`
   query GetUserByEmail($email: String) {
-    sh_users(where: {email: {_eq: $email}}) {
+    sh_users(where: { email: { _eq: $email } }) {
       username
     }
   }
@@ -59,7 +59,7 @@ export const GET_USER_BY_EMAIL = gql`
 // get person id by user mail
 export const GET_PERSON_ID_BY_USER_EMAIL = gql`
   query GetPersonIdByUserEmail($email: String) {
-    sh_users(where: {email: {_eq: $email}}) {
+    sh_users(where: { email: { _eq: $email } }) {
       persons_id
     }
   }
@@ -67,7 +67,7 @@ export const GET_PERSON_ID_BY_USER_EMAIL = gql`
 
 export const GET_STUDENT_ID_BY_USER_EMAIL = gql`
   query GetStudentIdByUserEmail($email: String) {
-    sh_users(where: {email: {_eq: $email}}) {
+    sh_users(where: { email: { _eq: $email } }) {
       person {
         students {
           id
@@ -80,28 +80,38 @@ export const GET_STUDENT_ID_BY_USER_EMAIL = gql`
 // get if user is enabled
 export const GET_USER_STATUS_BY_EMAIL = gql`
   query GetUserStatusByEmail($email: String) {
-    sh_users(where: {email: {_eq: $email}}) {
+    sh_users(where: { email: { _eq: $email } }) {
       user_status
     }
   }
 `;
 
 export const GET_OWNER_BY_ID = gql`
-query GetOwnerById($id: Int) {
-  sh_users(where: {id: {_eq: $id}}) {
-    person {
-      owners {
-        id
+  query GetOwnerById($id: Int) {
+    sh_users(where: { id: { _eq: $id } }) {
+      person {
+        owners {
+          id
+        }
       }
     }
   }
-}
 `;
 
 export const GET_SN_VALIDATION = gql`
   query GetSnValidation {
     sh_users {
       created_with_sn
+    }
+  }
+`;
+
+export const GET_SHARED_BY_PERSON_ID = gql`
+  query GetSharedByPersonId($id: Int) {
+    sh_persons(where: { id: { _eq: $id } }) {
+      students {
+        shared
+      }
     }
   }
 `;
