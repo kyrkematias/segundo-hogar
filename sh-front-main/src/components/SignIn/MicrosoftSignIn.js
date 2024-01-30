d31ce5474329e58b20609271a4d8369b61beb5af
kyrkematias <martin_matias@hotmail.com.ar>
2024-01-30 19:33:37 -0300

    add confirmation toast to social sign in

--- sh-front-main/src/components/SignIn/MicrosoftSignIn.js
+++ sh-front-main/src/components/SignIn/MicrosoftSignIn.js
@@ -16,15 +16,13 @@ import { USER_CATEGORIES } from "const";
 import { getAuth, signInWithPopup, OAuthProvider } from "firebase/auth";
 import { GET_STUDENT_USER_BY_ID } from "client/gql/queries/users";
 import { GET_USER_BY_EMAIL } from "client/gql/queries/users";
-import {
-  useToast,
-} from '@chakra-ui/react'
+import { useToast } from "@chakra-ui/react";
 
 const MicrosoftSignIn = () => {
   const toast = useToast();
 
   const { onSubmitLogginWithSocialNet } = useLoginWithSocialNet();
-  const provider = new OAuthProvider('microsoft.com');
+  const provider = new OAuthProvider("microsoft.com");
   const [_, setLocation] = useLocation();
   const [registerStudentUser] = useMutation(REGISTER_STUDENT_USER_WITH_SOC_NET);
   const [initialRegisterStudentUser] = useMutation(
@@ -76,6 +74,13 @@ const MicrosoftSignIn = () => {
             });
 
             dispatch(signInSocialNetAction(user.email));
+            toast({
+              title: "Registrado con éxito.",
+              description: "El usuario se ha registrado con éxito.",
+              status: "success",
+              duration: 3000,
+              isClosable: true,
+            });
             console.log("está registrando");
             setLocation(paths.completeProfile);
           }
@@ -95,8 +100,9 @@ const MicrosoftSignIn = () => {
       }
     } catch (error) {
       console.error("Error al iniciar sesión con Microsoft:", error);
-      if (error.code === 'auth/account-exists-with-different-credential') {
-        const loginError = "Ya existe una cuenta con el mismo correo electrónico pero con diferente credencial de inicio de sesión. Intente iniciar sesión con una credencial diferente.";
+      if (error.code === "auth/account-exists-with-different-credential") {
+        const loginError =
+          "Ya existe una cuenta con el mismo correo electrónico pero con diferente credencial de inicio de sesión. Intente iniciar sesión con una credencial diferente.";
         // opent toast if error
         toast({
           title: "Error al iniciar sesión",
@@ -105,7 +111,7 @@ const MicrosoftSignIn = () => {
           duration: 5000,
           isClosable: true,
         });
-      } 
+      }
     }
   };
 
@@ -120,7 +126,7 @@ const MicrosoftSignIn = () => {
 
       console.log(data);
       console.log("existe person? ", data?.sh_users.length > 0);
-      return data?.sh_users.length > 0; 
+      return data?.sh_users.length > 0;
     } catch (error) {
       console.error("Error al verificar el correo electrónico:", error);
       return false;
