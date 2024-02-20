548afecae299d073c92eeaca7efb63ed2155dbcb
kyrkematias <martin_matias@hotmail.com.ar>
2024-02-20 17:59:13 -0300

    toast for confirmation in tags and register publication added

--- sh-front-main/src/hooks/pages/PublicationRegister/usePublicationRegister.js
+++ sh-front-main/src/hooks/pages/PublicationRegister/usePublicationRegister.js
@@ -1,26 +1,22 @@
-import { useMutation } from '@apollo/client';
-import { useDispatch } from 'react-redux';
-import { useForm } from 'react-hook-form';
-import { REGISTER_PUBLICATION } from 'client/gql/mutations/registerPublication/registerPublication';
+import { useMutation } from "@apollo/client";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { REGISTER_PUBLICATION } from "client/gql/mutations/registerPublication/registerPublication";
 import { paths } from "config/paths";
-import useLocation from 'wouter/use-location';
+import useLocation from "wouter/use-location";
 import { useInitialPublications } from "hooks/pages/Search/useInitialPublications";
 import { authSelector } from "store/slices/authSlice";
 import { useSelector } from "react-redux";
+import { useToast } from "@chakra-ui/react";
 
-
-
-// Define la acción para crear una nueva publicación
 export const createPublicationAction = (data) => ({
-  type: 'CREATE_PUBLICATION',
+  type: "CREATE_PUBLICATION",
   payload: data,
 });
 
-// Función de hook para el formulario de registro de publicación
 export function useRegisterPublicationForm() {
   const dispatch = useDispatch();
 
-  // Importa la mutación de Apollo Client
   const [registerPublication, { loading }] = useMutation(REGISTER_PUBLICATION);
 
   const {
@@ -31,10 +27,10 @@ export function useRegisterPublicationForm() {
 
   const [_, setLocation] = useLocation();
   const { publications, isError, isFetching } = useInitialPublications();
-  // const [errorsCaptcha, setErrorsCaptcha] = useState({ message: '' });
-  // const [validCaptcha, setValidCaptcha] = useState(false);
+
   const { user } = useSelector(authSelector);
   const ownershipId = localStorage.getItem("ownershipId");
+  const toast = useToast();
   const onSubmit = async (data) => {
     try {
       console.log("price:", data.price);
@@ -50,7 +46,7 @@ export function useRegisterPublicationForm() {
           price: data.price,
           contact_name: data.fullname,
           contact_phone: data.phone,
-          contact_email: data.email, 
+          contact_email: data.email,
           publication_state: true,
         },
       });
@@ -59,19 +55,37 @@ export function useRegisterPublicationForm() {
         dispatch(onSubmit(result.data.registerPublication));
         localStorage.removeItem(ownershipId);
         setLocation(`/cuenta/${user.id}`);
+        toast({
+          title: "¡Publicación registrada con éxito!",
+          description: "La publicación se ha registrado correctamente.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
       } else {
         console.error("La mutación no devolvió datos válidos.");
         setLocation(`/cuenta/${user.id}`);
+        toast({
+          title: "Error al registrar la publicación",
+          description:
+            "Algo salió mal al intentar crear la publicación. Intenta más tarde o contacta a soporte.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
       }
     } catch (error) {
       console.error("Error al registrar la publicación:", error.message);
       setLocation(`/cuenta/${user.id}`);
+      toast({
+        title: "Error al registrar la publicación",
+        description: error.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
     }
   };
-
-  // function onChange(value) {
-  //   value ? setValidCaptcha(true) : setValidCaptcha(false);
-  // }
 
   return {
     register,
--- sh-front-main/src/store/slices/recommSlice.js
+++ sh-front-main/src/store/slices/recommSlice.js
@@ -1,82 +1,99 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { recomm } from "services";
+import { useToast } from "@chakra-ui/react";
 
 const initialState = {
-    userCreated: null,
-    isFetching: false,
-    isSuccess: false,
-    isError: false,
-    errorMessage: "",
+  userCreated: null,
+  isFetching: false,
+  isSuccess: false,
+  isError: false,
+  errorMessage: "",
 };
 
 export const recommSlice = createSlice({
-    name: "recomm",
-    initialState,
-    reducers: {
-        successUserCreated: (state, action) => {
-            state.userCreated = action.payload;
-            state.isFetching = false;
-            state.isSuccess = true;
-        },
-        recommLoading: (state) => {
-            if (state.isFetching === false) {
-                state.isFetching = true;
-            }
-        },
-        success: (state, action) => {
-            state.isFetching = false;
-            state.isSuccess = true;
-        },
-        failed: (state, action) => {
-            state.isFetching = false;
-            state.isError = true;
-            state.errorMessage = action.payload.error;
-        },
-        clearState: (state, action) => {
-            state.userCreated = null;
-            state.isSuccess = false;
-            state.isError = false;
-            state.errorMessage = "";
-        },
+  name: "recomm",
+  initialState,
+  reducers: {
+    successUserCreated: (state, action) => {
+      state.userCreated = action.payload;
+      state.isFetching = false;
+      state.isSuccess = true;
     },
+    recommLoading: (state) => {
+      if (state.isFetching === false) {
+        state.isFetching = true;
+      }
+    },
+    success: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+    },
+    failed: (state, action) => {
+      state.isFetching = false;
+      state.isError = true;
+      state.errorMessage = action.payload.error;
+    },
+    clearState: (state, action) => {
+      state.userCreated = null;
+      state.isSuccess = false;
+      state.isError = false;
+      state.errorMessage = "";
+    },
+  },
 });
 
 export const {
-    successUserCreated,
-    recommLoading,
-    success,
-    failed,
-    clearState,
+  successUserCreated,
+  recommLoading,
+  success,
+  failed,
+  clearState,
 } = recommSlice.actions;
 
 export const createNodeAction = (data) => async (dispatch) => {
-    dispatch(recommLoading());
-    try {
-        window.localStorage.setItem("createdIdPerson", data.id);
-        await recomm.createNodeService({ user: data });
-        dispatch(successUserCreated());
-    } catch (err) {
-        console.error(err);
-        dispatch(failed(err));
-    }
+  dispatch(recommLoading());
+  try {
+    window.localStorage.setItem("createdIdPerson", data.id);
+    await recomm.createNodeService({ user: data });
+    dispatch(successUserCreated());
+  } catch (err) {
+    console.error(err);
+    dispatch(failed(err));
+  }
 };
 
 export const createRelationsAction = (data) => async (dispatch) => {
-    dispatch(recommLoading());
-    try {
-        const idPerson = getCreatedIdPerson();
-        await recomm.createRelationsService({ idPerson, relations: data });
-        window.localStorage.removeItem("createdIdPerson");
-        window.localStorage.removeItem("questionsValue");
-        dispatch(clearState());
-    } catch (err) {
-        console.error(err);
-        dispatch(failed(err));
-    }
+  const toast = useToast();
+  dispatch(recommLoading());
+  try {
+    const idPerson = getCreatedIdPerson();
+    await recomm.createRelationsService({ idPerson, relations: data });
+    window.localStorage.removeItem("createdIdPerson");
+    window.localStorage.removeItem("questionsValue");
+    dispatch(clearState());
+    toast({
+      title: "¡Registrado con éxito!",
+      description: "Se han guardado tus preferencias con éxito.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  } catch (err) {
+    console.error(err);
+    dispatch(failed(err));
+    toast({
+      title: "¡Ups! Algo salió mal.",
+      description:
+        "Por favor, inténtalo de nuevo más tarde o contacta a soporte.",
+      status: "error",
+      duration: 3000,
+      isClosable: true,
+    });
+  }
 };
 
 function getCreatedIdPerson() {
-    return window.localStorage.getItem("createdIdPerson");
+  return window.localStorage.getItem("createdIdPerson");
 }
 
 export default recommSlice.reducer;
